be2d78def4f25c2965600269f3deb736b64d0be0
lksbh <lks_bh@hotmail.com>
2015-10-16 02:03:05 -0300

    converter o pdf pra qual formato?
    falta manipullar o "web unit" para se fazer a conversa com o site da
    impresa e consegir os dados

--- getfiles/MainWindow.xaml.cs
+++ getfiles/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.IO;
+using System.Net;
+using iTextSharp.text.pdf.parser;
+
 
 
 namespace getfiles
@@ -24,13 +27,31 @@ namespace getfiles
     /// </summary>
     public partial class MainWindow : Window
     {
+        int ValorDaPagina;
+        int ValorDoPDF;
+        
+            string valor1;
+                string valor2 ;
         public MainWindow()
         {
             InitializeComponent();
+            ValorDaPagina = 0;
+            ValorDoPDF = 0;
+            label1.Content = "!";
+
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+
+        public void carega_valores() 
         {
+             valor1 = "http://jornal.iof.mg.gov.br/xmlui/bitstream/handle/123456789/";
+             valor2 = "http://jornal.iof.mg.gov.br/xmlui/bitstream/handle/123456789/153240/caderno1_2015-10-15%";
+            ValorDaPagina = Convert.ToInt32(CAIXA1.Text.Substring( valor1.Length ,6));
+            ValorDoPDF = Convert.ToInt32(CAIXA1.Text.Substring( valor2.Length ,3));
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        { 
+            carega_valores();
             FolderBrowserDialog dialog = new FolderBrowserDialog();
            
             System.Windows.Forms.DialogResult result = dialog.ShowDialog();
@@ -40,10 +61,53 @@ namespace getfiles
                 // The user selected a folder and pressed the OK button.
                 // We print the number of files found.
                 //,
-              
+
+                //verifica e cria a treta da pasta
+                if (!Directory.Exists(dialog.SelectedPath)) Directory.CreateDirectory(dialog.SelectedPath);
+
+                //conta os arkivos num diretorio
+                label1.Content = dialog.SelectedPath.ToString();
                 string[] files = Directory.GetFiles(dialog.SelectedPath);
                 System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+
+                // "le a pagina da web ate agora ta uma bosta
+
+                WebRequest req = WebRequest.Create("http://www.iof.mg.gov.br/index.php?/ultima-edicao.html");
+                WebResponse res = req.GetResponse();
+                Stream dataStream = res.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string resposta = reader.ReadToEnd();
+                
+                reader.Close();
+                res.Close();
+
+
+                //escreve o arquivo txt diretao
+                using (StreamWriter writer = new StreamWriter(dialog.SelectedPath + "\\TESTE.txt"))
+                {
+                    writer.Write("Lucas Souza .net ");
+                    writer.Write(resposta);
+                 
+                }
+
+                using (var client = new WebClient())
+                {
+                    //trocar o nome do arkivo pro nome da pagina vai ser util pra carai!!!
+                    client.DownloadFile("http://jornal.iof.mg.gov.br/xmlui/bitstream/handle/123456789/" + ValorDaPagina + "/caderno1_2015-10-15%"+ValorDoPDF+".pdf", dialog.SelectedPath + "\\"+ valor2.Substring(valor1.Length+7)+ ".pdf");
+                }
+
+
             }
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        public void Geratexto()
+        {
+            PdfTextExtractor.GetTextFromPage(new Pd);
         }
     }
 }
